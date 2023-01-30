f5ad6d7af0bad36e01180398090bea0d146f4938
Adam White <adamfrankwhite@gmail.com>
2023-01-30 00:32:09 +0000

    added new image paths/captions

--- script.js
+++ script.js
@@ -40,6 +40,57 @@ const photos = [
         url: "assets/portfolio/IMG_7682.jpg",
         caption: "Group of mallards | Reddish Vale, Stockport | January 2023",
     },
+
+    {
+        url: "assets/portfolio/IMG_7729.jpg",
+        caption:
+            "Group of oystercatchers feeding | Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7735.jpg",
+        caption: " | Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7746.jpg",
+        caption: "Curlew looking for food | Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7768.jpg",
+        caption:
+            "Oystercatcher looking for food in the rocks | Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7772.jpg",
+        caption: "Lapwing | Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7788-2.jpg",
+        caption: "Oystercatcher | Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7808.jpg",
+        caption: " | Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7890.jpg",
+        caption: "Pied wagtail | Hest Bank, Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7919.jpg",
+        caption: "Pied wagtail | Hest Bank, Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7936.jpg",
+        caption: "Unidentified formation | Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7942.jpg",
+        caption: " | Morecambe Bay | January 2023",
+    },
+    {
+        url: "assets/portfolio/IMG_7951.jpg",
+        caption: "Pheasant | Silverdale | January 2023",
+    },
 ];
 
 photos.forEach((photo) => {
